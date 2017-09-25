88b1929f245a04bdc50342d67a3a0a9466b8fd79
honjovi <honjovi.tarou@gmail.com>
2017-09-26 00:26:15 +0900

    Add saving PNG file.

--- scripts/index.js
+++ scripts/index.js
@@ -4,46 +4,105 @@
 var electron = require('electron');
 var remote = electron.remote;
 var dialog = remote.dialog;
+var fs = require('fs');
 var psd = require('psd');
 var PNG = require('pngjs').PNG;
 
 
-$('#file_select').on('click', function(){
-	var currentWindow = remote.getCurrentWindow();
-
-	dialog.showOpenDialog(
-		currentWindow, 
-		{
-			properties: ['openFile'],
-			filters: [{ name: 'PSD File', extensions: ['psd']}]
-		},
-		function(files){
-			//cancel
-			if(!files || files.length === 0){
-				return;
-			}
-			
-			$('#file_select').addClass('disabled');
-			
-			openPSDFile(files[0]);
+$('#select-file').on('click', function(){
+	var options = {
+		properties: ['openFile'],
+		filters: [{ name: 'PSD File', extensions: ['psd']}]
+	};
+	var callback = function(files){
+		//cancel
+		if(!files || files.length === 0){
+			return;
 		}
-	);
+		
+		$('#select-file').addClass('disabled');
+		
+		openPSDFile(files[0]);
+	}
+	
+	dialog.showOpenDialog(null, options, callback);
 });
+
+
+$('#save-img').on('click', function(){
+	var imgCanvas = $('#img-canvas').get(0);
+	var positionCanvas = $('#position-canvas').get(0);
+	$('#save-canvas')
+		.attr('width', imgCanvas.width.toString())
+		.attr('height', imgCanvas.height.toString());
+	var saveCanvas = $('#save-canvas').get(0);
+	var saveCtx = saveCanvas.getContext('2d');
+	
+	saveCtx.clearRect(0, 0, saveCanvas.width, saveCanvas.height);
+	
+	var imgElm = getImageFromCanvas(imgCanvas);
+	var positionElm = getImageFromCanvas(positionCanvas);
+	
+	drawImageToCanvas2(imgElm, saveCtx, function(){
+		drawImageToCanvas2(positionElm, saveCtx, function(){
+			var data = saveCanvas.toDataURL('image/png').replace(/^data:image\/png;base64,/, "");
+			savePNG(data);
+		});
+	});
+});
+
+
+var savePNG = function(data){
+	var options = {
+		title: '保存',
+		filters: [{name: 'PNGファイル', extensions: ['png']}]
+	};
+	
+	var callback = function(filename){
+		fs.writeFile(filename, data, 'base64', function (err) {
+			if (err) {
+				return console.error(err);
+			}
+		});
+	}
+	
+	dialog.showSaveDialog(null, options, callback);
+}
+
+
+var drawImageToCanvas2 = function(img, ctx, cb){
+	if(img.complete){
+		ctx.drawImage(img, 0, 0);
+		cb();
+	}else{
+		img.onload = function(){
+			ctx.drawImage(img, 0, 0);
+			cb();
+		};
+	}
+}
+
+
+var getImageFromCanvas = function(canvas){
+	var image = new Image();
+	image.src = canvas.toDataURL();
+	return image;
+}
 
 
 var openPSDFile = function(file){
 	var psd_file = psd.fromFile(file);
 	psd_file.parse();
-	console.log(psd_file);
 	
 	createCanvas(psd_file.image);
-	
 	createLayerTable();
 	
 	forAllLayer(psd_file.tree(), function(node){
 		appendLayerToTable(node.layer);
 		appendCooridanteToCanvas(node.layer.left, node.layer.top);
 	});
+	
+	$('#save-block').show();
 };
 
 
@@ -51,30 +110,16 @@ var createCanvas = function(image){
 	var imageElm = createImageElement(image);
 	$('#img-canvas')
 		.attr('width', image.width().toString())
-		.attr('height', image.height().toString())
-		.show();
+		.attr('height', image.height().toString());
 	
 	$('#position-canvas')
 		.attr('width', image.width().toString())
-		.attr('height', image.height().toString())
-		.show();
+		.attr('height', image.height().toString());
 	
-	if(imageElm.complete){
-		drawImageToCanvas(imageElm);
-	}else{
-		//createProgressBar();
-		imageElm.onload = function(e){
-			drawImageToCanvas(e.target);
-			//deleteProgressBar();
-		};
-	}
+	drawImageToCanvas2(imageElm, $('#img-canvas').get(0).getContext('2d'), function(){
+		$('#img-block').show();
+	});
 };
-
-
-var drawImageToCanvas = function(image){
-	var ctx = $('#img-canvas').get(0).getContext('2d');
-	ctx.drawImage(image, 0, 0);
-}
 
 
 var createProgressBar = function(){
@@ -144,6 +189,7 @@ var createImageElement = function(layerImage){
 	imageElm.src = "data:image/png;base64," + b64Data;
 	return imageElm;
 };
+
 
 var appendCooridanteToCanvas = function(x, y){
 	var ctx = $('#position-canvas').get(0).getContext('2d');
